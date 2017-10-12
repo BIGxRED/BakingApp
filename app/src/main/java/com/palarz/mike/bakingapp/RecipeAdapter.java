d882ad569df9aaf88953ba5f50a0df2b2caa7e64
Mike Palarz <mpalarz1@gmail.com>
2017-10-12 17:26:56 -0500

    Intermediate work on the recipe details activity; adjustments to the RecipeAdapter

--- app/src/main/java/com/palarz/mike/bakingapp/RecipeAdapter.java
+++ app/src/main/java/com/palarz/mike/bakingapp/RecipeAdapter.java
@@ -3,12 +3,17 @@ package com.palarz.mike.bakingapp;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by mpala on 9/30/2017.
@@ -16,7 +21,18 @@ import java.util.List;
 
 public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder> {
 
+    private static final String TAG = RecipeAdapter.class.getSimpleName();
+
     public static final String EXTRA_RECIPE = "com.palarz.mike.bakingapp.recipe";
+
+    public static final int[] IMAGE_RESOURCES = new int[]{R.drawable.recipe1,
+                                                            R.drawable.recipe2,
+                                                            R.drawable.recipe3,
+                                                            R.drawable.recipe4,
+                                                            R.drawable.recipe5,
+                                                            R.drawable.recipe6,
+                                                            R.drawable.recipe7,
+                                                            R.drawable.recipe8};
 
     private List<Recipe> mRecipeList;
     private Context mContext;
@@ -37,7 +53,14 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeView
     @Override
     public void onBindViewHolder(RecipeViewHolder recipeViewHolder, int position) {
         Recipe recipe = mRecipeList.get(position);
+
         recipeViewHolder.vName.setText(recipe.getName());
+
+        //TODO: Maybe find a placeholder image for when the image is attempted to being loaded?
+        Picasso.with(mContext)
+                .load(recipe.getImage())
+                .error(getRandomImageResource())
+                .into(recipeViewHolder.vImage);
     }
 
     @Override
@@ -56,13 +79,14 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeView
 
 
     public class RecipeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        protected TextView vName;
+        public TextView vName;
+        public ImageView vImage;
 
-
-        public RecipeViewHolder(View v) {
-            super(v);
-            vName =  (TextView) v.findViewById(R.id.recipe_name);
-            vName.setOnClickListener(this);
+        public RecipeViewHolder(View view) {
+            super(view);
+            vName = view.findViewById(R.id.recipe_list_item_name);
+            vImage = view.findViewById(R.id.recipe_list_item_image);
+            view.setOnClickListener(this);
         }
 
         @Override
@@ -72,5 +96,12 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeView
             makingTheRecipeIntent.putExtra(EXTRA_RECIPE, clickedRecipe);
             mContext.startActivity(makingTheRecipeIntent);
         }
+    }
+
+    public int getRandomImageResource(){
+        int randomIndex = new Random().nextInt(IMAGE_RESOURCES.length);
+        Log.d(TAG, "Result of random index: " + randomIndex);
+
+        return IMAGE_RESOURCES[randomIndex];
     }
 }
